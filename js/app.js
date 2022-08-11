570592e5e5551d7f87138de80c65aac33dd7cd5a
Stark <serj.elessar@gmail.com>
2022-08-11 10:45:46 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   const accordionItems = document.querySelectorAll('.accordion__item')
+  const accordionContainer = document.querySelector('.accordion')
 
   accordionItems.forEach((item, index) => {
+    const header = item.querySelector('.accordion__header')
+    const content = item.querySelector('.accordion__content')
+    const icon = item.querySelector('.accordion__icon')
+
+    if (item.hasAttribute('data-open')) {
+      item.classList.add('open')
+      content.style.height = `${content.scrollHeight}px`
+      icon.classList.replace('hide', 'show')
+    }
     const colorBackground = item.dataset.colorBack
     const colorBorder = item.dataset.colorBorder
-    
+
     if (colorBackground || colorBorder) {
       item.style.background = colorBackground
       item.style.borderColor = colorBorder
     }
-    const header = item.querySelector('.accordion__header')
 
     header.addEventListener('click', () => {
-      const content = item.querySelector('.accordion__content')
-      const icon = item.querySelector('.accordion__icon')
-
       item.classList.toggle('open')
       if (item.classList.contains('open')) {
         content.style.height = `${content.scrollHeight}px`
@@ -24,7 +30,9 @@ document.addEventListener('DOMContentLoaded', () => {
         icon.classList.replace('show', 'hide')
       }
 
-      removeOpen(index)
+      if (accordionContainer.dataset.autoClosing === 'true') {
+        removeOpen(index)
+      }
     })
   })
 
